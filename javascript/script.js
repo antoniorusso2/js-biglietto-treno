4f57e79e4c8049a7c04d8ed0546500c9498fefea
antoniorusso2 <russoantonio303@outlook.it>
2024-10-14 17:19:08 +0200

    FIX: changed floating numbers property from 'Math.round(n*100)/100' with 'toFixed(2)' and included it in parseFloat for result as a number

--- javascript/script.js
+++ javascript/script.js
@@ -11,37 +11,42 @@
   
   //richiesta numero km da percorrere
     //prompt richiesta distanza espressa in km
-  let tripDistance = parseFloat(prompt('Inserisci adesso la distanza del viaggio in KM')); //number
-  console.log(Number(tripDistance));
+  let tripDistance = Number(parseFloat(prompt('Inserisci adesso la distanza del viaggio in KM')).toFixed(2)); //number
+  // console.log(typeof tripDistance);
+  console.log(tripDistance);
 
 //PREZZO BASE
   //prezzo totale per la distanza da percorrere calcolato in base alle info fornite dal cliente:
     // 1. 1km = 0.21€ ;
-  let kmPrice = tripDistance * 0.21; 
-  let kmRoundedPrice = Math.round(kmPrice*100)/100//number
-  console.log(`Il prezzo basato sulla distanza per questo biglietto è di ${kmRoundedPrice} €`); //string
+  let kmPrice = parseFloat((tripDistance * 0.21).toFixed(2)); //number
+  console.log(`Il prezzo basato sulla distanza per questo biglietto è di ${kmPrice} €`);
+  // console.log(typeof kmPrice) //number
   // console.log(typeof kmPrice) 
 
 //SCONTI DA APPLICARE  
   let discount ;
-  let discountedPrice ;
     // 2. se eta' < 18 = 20% di sconto
     if (userAge < 18) {
-      discount = kmPrice * 20 / 100; //number
-      discountedPrice = Math.round(kmPrice * 100) / 100;// number
+      discount = parseFloat((kmPrice * 20 / 100).toFixed(2)); //number
     }
     // 3. se eta' > di 65 = 40% di sconto
     else if (userAge > 65) {
-      discount = kmPrice * 40 / 100; // number
-      discountedPrice = Math.round(kmPrice * 100) / 100;// number
+      discount = parseFloat((kmPrice * 40 / 100).toFixed(2)) // number
     }
-  console.log(`In base all\'eta\' del passeggero verrà applicato uno sconto pari a ${discountedPrice} €`);
+    else {
+      discount = 0;
+    }
+  console.log(`In base all\'eta\' del passeggero verrà applicato uno sconto pari a ${discount} €`);
+  // console.log(typeof discount) //number
 
 
 //OUTPUT
   //prezzo con max 2 decimali
-  let finalPrice = Math.round((kmPrice - discount) * 100) / 100 ;
+  let discountedPrice = parseFloat((kmPrice - discount).toFixed(2)) ; //number
+  let noDiscountPrice = kmPrice; //number
 
-  console.log(`Il prezzo finale è pari a ${finalPrice} €`);
+  //console.log(typeof userAge, typeof tripDistance, typeof kmPrice, typeof discount, typeof discountedPrice, typeof noDiscountPrice) //all price and data result as a number
+
+  console.log(`Il prezzo finale è pari a ${discountedPrice} €`);
   
  
