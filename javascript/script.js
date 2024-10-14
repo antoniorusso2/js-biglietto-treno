fac38212a30289b1891c6952aa15cd5bececa6ef
antoniorusso2 <russoantonio303@outlook.it>
2024-10-14 23:13:52 +0200

    removed parseFloat and toFixed for all the calc and leaved it only for the prompt and the results

--- javascript/script.js
+++ javascript/script.js
@@ -5,33 +5,33 @@
 //DATI UTENTE
   //eta' del passeggero
   //prompt eta' del cliente con ritorno dati di tipo "number"
-  let userAge = parseInt(prompt('Benvenuto/a, per il calcolo del preventivo inserisci qui di sotto l\'eta\'del passeggero')); //number
-  // console.log(typeof userAge);
+  let userAge = parseFloat(prompt('Benvenuto/a, per il calcolo del preventivo inserisci qui di sotto l\'eta\'del passeggero')); //number
+  // console.log(typeof userAge); //number
   console.log(userAge);
   
   //richiesta numero km da percorrere
     //prompt richiesta distanza espressa in km
-  let tripDistance = Number(parseFloat(prompt('Inserisci adesso la distanza del viaggio in KM')).toFixed(2)); //number
-  // console.log(typeof tripDistance);
+  let tripDistance = parseFloat(prompt('Inserisci adesso la distanza del viaggio in KM')); //number
+  //console.log(typeof tripDistance); //number
   console.log(tripDistance);
 
 //PREZZO BASE
   //prezzo totale per la distanza da percorrere calcolato in base alle info fornite dal cliente:
     // 1. 1km = 0.21€ ;
-  let kmPrice = parseFloat((tripDistance * 0.21).toFixed(2)); //number
+  let kmPrice = (tripDistance * 0.21); 
   console.log(`Il prezzo basato sulla distanza per questo biglietto è di ${kmPrice} €`);
-  // console.log(typeof kmPrice) //number
+  //console.log(typeof kmPrice) //number
   // console.log(typeof kmPrice) 
 
 //SCONTI DA APPLICARE  
   let discount ;
     // 2. se eta' < 18 = 20% di sconto
     if (userAge < 18) {
-      discount = parseFloat((kmPrice * 20 / 100).toFixed(2)); //number
+      discount = kmPrice * 20 / 100; //number
     }
     // 3. se eta' > di 65 = 40% di sconto
     else if (userAge > 65) {
-      discount = parseFloat((kmPrice * 40 / 100).toFixed(2)) // number
+      discount = kmPrice * 40 / 100; // number
     }
     else {
       discount = 0;
