d8c4d4108bb2b915d376163b912bcccb8cb1265a
antoniorusso2 <russoantonio303@outlook.it>
2024-10-14 16:42:18 +0200

    ADD: max 2 floating number for all prices making it more accurate

--- javascript/script.js
+++ javascript/script.js
@@ -11,32 +11,36 @@
   
   //richiesta numero km da percorrere
     //prompt richiesta distanza espressa in km
-  let tripDistance = parseInt(prompt('Inserisci adesso la distanza del viaggio in KM')); //number
-  console.log(tripDistance);
+  let tripDistance = parseFloat(prompt('Inserisci adesso la distanza del viaggio in KM')); //number
+  console.log(Number(tripDistance));
 
 //PREZZO BASE
   //prezzo totale per la distanza da percorrere calcolato in base alle info fornite dal cliente:
     // 1. 1km = 0.21€ ;
-  let kmPrice = tripDistance * 0.21; //number
-  console.log(`Il prezzo basato sulla distanza per questo biglietto è di ${kmPrice} €`); //string
+  let kmPrice = tripDistance * 0.21; 
+  let kmRoundedPrice = Math.round(kmPrice*100)/100//number
+  console.log(`Il prezzo basato sulla distanza per questo biglietto è di ${kmRoundedPrice} €`); //string
   // console.log(typeof kmPrice) 
 
 //SCONTI DA APPLICARE  
   let discount ;
+  let discountedPrice ;
     // 2. se eta' < 18 = 20% di sconto
     if (userAge < 18) {
-      discount = kmPrice * 20 / 100; // number
+      discount = kmPrice * 20 / 100; //number
+      discountedPrice = Math.round(kmPrice * 100) / 100;// number
     }
     // 3. se eta' > di 65 = 40% di sconto
     else if (userAge > 65) {
       discount = kmPrice * 40 / 100; // number
+      discountedPrice = Math.round(kmPrice * 100) / 100;// number
     }
-  console.log(`In base all\'eta\' del passeggero verrà applicato uno sconto pari a ${discount} €`);
+  console.log(`In base all\'eta\' del passeggero verrà applicato uno sconto pari a ${discountedPrice} €`);
 
 
 //OUTPUT
   //prezzo con max 2 decimali
-  let finalPrice = kmPrice - discount ;
+  let finalPrice = Math.round((kmPrice - discount) * 100) / 100 ;
 
   console.log(`Il prezzo finale è pari a ${finalPrice} €`);
   
