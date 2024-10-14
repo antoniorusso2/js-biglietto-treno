d437685d1a32e1cc51b2bac8399d75e5eaca7486
antoniorusso2 <russoantonio303@outlook.it>
2024-10-14 16:14:18 +0200

    ADD: km pricing + age based discounts

--- javascript/script.js
+++ javascript/script.js
@@ -1,22 +1,43 @@
 //BIGLIETTO PER IL TRENO
 
 //DATI PER IL CALCOLO DEL PREVENTIVO
-//eta' del passeggero
-  //prompt eta' del cliente con ritorno dati di tipo "number"
-  let userAge = parseInt(prompt('Benvenuto/a, per il calcolo del preventivo inserisci qui di sotto l\'eta\'del passeggero'));
 
+//DATI UTENTE
+  //eta' del passeggero
+  //prompt eta' del cliente con ritorno dati di tipo "number"
+  let userAge = parseInt(prompt('Benvenuto/a, per il calcolo del preventivo inserisci qui di sotto l\'eta\'del passeggero')); //number
   // console.log(typeof userAge);
   console.log(userAge);
   
   //richiesta numero km da percorrere
     //prompt richiesta distanza espressa in km
-  let tripDistance = parseInt(prompt('Inserisci adesso la distanza del viaggio in KM'));
+  let tripDistance = parseInt(prompt('Inserisci adesso la distanza del viaggio in KM')); //number
   console.log(tripDistance);
-  
-  //prezzo totale calcolato in base alle info fornite dal cliente:
+
+//PREZZO BASE
+  //prezzo totale per la distanza da percorrere calcolato in base alle info fornite dal cliente:
     // 1. 1km = 0.21€ ;
-    // 2. 20% di sconto se eta' < 18;
-    // 3. 40% di sconto se eta' > 65;
-  
-  //OUTPUT
+  let kmPrice = tripDistance * 0.21; //number
+  console.log(`Il prezzo basato sulla distanza per questo biglietto è di ${kmPrice} €`); //string
+  // console.log(typeof kmPrice) 
+
+//SCONTI DA APPLICARE  
+  let discount ;
+    // 2. se eta' < 18 = 20% di sconto
+    if (userAge < 18) {
+      discount = kmPrice * 20 / 100; // number
+    }
+    // 3. se eta' > di 65 = 40% di sconto
+    else if (userAge > 65) {
+      discount = kmPrice * 40 / 100; // number
+    }
+  console.log(`In base all\'eta\' del passeggero verrà applicato uno sconto pari a ${discount} €`);
+
+
+//OUTPUT
   //prezzo con max 2 decimali
+  let finalPrice = kmPrice - discount ;
+
+  console.log(`Il prezzo finale è pari a ${finalPrice} €`);
+  
+ 
